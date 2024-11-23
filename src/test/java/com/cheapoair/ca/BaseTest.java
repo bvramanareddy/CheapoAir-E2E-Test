e3224fd723a0fc60a88f8209c970b26225b4e56f
Ramana <v_boda@yahoo.com>
2024-11-23 08:59:35 -0500

    old code removed and new code appended a little to adjust s

--- src/test/java/com/cheapoair/ca/BaseTest.java
+++ src/test/java/com/cheapoair/ca/BaseTest.java
@@ -1,5 +1,7 @@
 package com.cheapoair.ca;
 
+import java.time.Duration;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
@@ -19,10 +21,10 @@ public class BaseTest {
 	public void setUp()
 	{
 	driver=  new ChromeDriver();
-	driver.manage().window().maximize();
+	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10)));
 	
 	
-	System.out.println("One line removed from the code and new line added here");
+	System.out.println("One line removed from the old code and new line added here");
 	}
 	
 	@AfterClass
